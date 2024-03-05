788e9cd849400c7a594430c56b3817df722dd7aa
Sebastian Feldmann <maker@sfeld.eu>
2024-03-05 03:33:00 +0000

    lower arm - forearm

--- pibsim_webots/pib_driver.py
+++ pibsim_webots/pib_driver.py
@@ -14,7 +14,8 @@ class PibDriver:
         self.__devices['elbow_right'] = self.__robot.getDevice('ellbow_right')
         self.__devices['elbow_left'] = self.__robot.getDevice('ellbow_left')
 
-        self.__devices['lower_arm_right'] = self.__robot.getDevice('forearm_right')
+        self.__devices['lower_arm_right_rotation'] = self.__robot.getDevice('forearm_right')
+        self.__devices['lower_arm_left_rotation'] = self.__robot.getDevice('forearm_left')
         self.__devices['thumb_left_stretch'] = [self.__robot.getDevice('thumb_left_distal'), self.__robot.getDevice('thumb_left_proximal'),]
         self.__devices['thumb_right_stretch'] = [self.__robot.getDevice('thumb_right_distal'),self.__robot.getDevice('thumb_right_proximal'),]
         self.__devices['index_right_stretch'] = [self.__robot.getDevice('index_right_distal'),self.__robot.getDevice('index_right_proximal'),]
